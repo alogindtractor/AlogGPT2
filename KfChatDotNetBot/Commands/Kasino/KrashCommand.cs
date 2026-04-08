4e292b2effe1cc5c8b50305eb4af53cf6f3ab1c9
A Log in D Tractor <230661157+alogindtractor@users.noreply.github.com>
2026-04-08 08:42:11 -0700

    Update KrashCommand.cs
    add wager limit

--- KfChatDotNetBot/Commands/Kasino/KrashCommand.cs
+++ KfChatDotNetBot/Commands/Kasino/KrashCommand.cs
@@ -28,7 +28,7 @@ public class KrashBetCommand : ICommand
         GroupCollection arguments,
         CancellationToken ctx)
     {
-        return;
+        
         var cleanupDelay = TimeSpan.FromSeconds(10);
         
         if (message is { IsWhisper: false, MessageUuid: not null })
@@ -62,10 +62,19 @@ public class KrashBetCommand : ICommand
             multi = Convert.ToDecimal(multiGroup.Value);
         }
         wager = Convert.ToDecimal(amountGroup.Value);
+        decimal wagerLimit = 10;
         if (wager > gambler.Balance)
         {
             await botInstance.SendChatMessageAsync(
                 $"{user.FormatUsername()}, your balance of {gambler.Balance} is not enough to bet {wager} on krash.",
+                true, autoDeleteAfter: TimeSpan.FromSeconds(5));
+            return;
+        }
+
+        if (wager > wagerLimit)
+        {
+            await botInstance.SendChatMessageAsync(
+                $"{user.FormatUsername()}, you can't bet more than {wagerLimit} on krash during testing.",
                 true, autoDeleteAfter: TimeSpan.FromSeconds(5));
             return;
         }
